afc0875dae1460b59a0e8242a2d72cd5d8526772
Fabián <fabilu242003@gmail.com>
2024-07-29 17:50:35 -0600

    Mostrar mensaje en el label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,38 +1,63 @@
-﻿namespace Tarea_Extraclase_1_2022155662;
-
-partial class Form1
+﻿namespace Tarea_Extraclase_1_2022155662
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private Button btnShowMessage;
+        private TextBox txtInput;
+        private Label lblMessage;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        private void InitializeComponent()
+        {
+            this.btnShowMessage = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShowMessage
+            // 
+            this.btnShowMessage.Location = new System.Drawing.Point(100, 100);
+            this.btnShowMessage.Name = "btnShowMessage";
+            this.btnShowMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnShowMessage.TabIndex = 0;
+            this.btnShowMessage.Text = "Mostrar mensaje";
+            this.btnShowMessage.UseVisualStyleBackColor = true;
+            this.btnShowMessage.Click += new System.EventHandler(this.btnShowMessage_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(100, 50);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(100, 150);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnShowMessage);
+            this.Name = "Form1";
+            this.Text = "Mi Aplicación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,19 @@
-namespace Tarea_Extraclase_1_2022155662;
+using System;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace Tarea_Extraclase_1_2022155662
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowMessage_Click(object sender, EventArgs e)
+        {
+            string inputText = txtInput.Text;
+            lblMessage.Text = inputText;
+        }
     }
 }
