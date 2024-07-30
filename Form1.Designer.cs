c369703850129c35caa7748df2dce599d0c186f4
Fabián <fabilu242003@gmail.com>
2024-07-29 23:55:30 -0600

    Creación de la textbox y Inputbox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -5,7 +5,10 @@
         private System.ComponentModel.IContainer components = null;
         private Button btnShowMessage;
         private TextBox txtInput;
-        private Label lblMessage;
+        private TextBox txtbox;
+        private Label lbltextbox;
+        private Label lblinputbox;
+        
 
         protected override void Dispose(bool disposing)
         {
@@ -20,42 +23,66 @@
         {
             this.btnShowMessage = new System.Windows.Forms.Button();
             this.txtInput = new System.Windows.Forms.TextBox();
-            this.lblMessage = new System.Windows.Forms.Label();
+            this.lbltextbox = new System.Windows.Forms.Label(); 
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.lblinputbox = new System.Windows.FormsLabel();
             this.SuspendLayout();
             // 
             // btnShowMessage
             // 
-            this.btnShowMessage.Location = new System.Drawing.Point(100, 100);
+            this.btnShowMessage.Location = new System.Drawing.Point(70, 490);
             this.btnShowMessage.Name = "btnShowMessage";
-            this.btnShowMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnShowMessage.Size = new System.Drawing.Size(200, 50);
             this.btnShowMessage.TabIndex = 0;
-            this.btnShowMessage.Text = "Mostrar mensaje";
+            this.btnShowMessage.Text = "Enviar";
             this.btnShowMessage.UseVisualStyleBackColor = true;
             this.btnShowMessage.Click += new System.EventHandler(this.btnShowMessage_Click);
+            this.btnShowMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(100, 50);
+            this.txtInput.Location = new System.Drawing.Point(40, 250);
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.Size = new System.Drawing.Size(270, 200);
             this.txtInput.TabIndex = 1;
+            this.txtInput.Multiline = true; // Permitir múltiples líneas
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical; // Barra de desplazamiento vertical
+            this.txtInput.WordWrap = true; // Asegura que el texto se ajuste al tamaño del TextBox
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown); // Suscribir al evento KeyDown
             // 
-            // lblMessage
+            // lbltextbox
             // 
-            this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(100, 150);
-            this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(0, 13);
-            this.lblMessage.TabIndex = 2;
+            this.lbltextbox.AutoSize = true; // Ajusta automáticamente el tamaño del Label
+            this.lbltextbox.Location = new System.Drawing.Point(350, 110); // Posición encima del TextBox
+            this.lbltextbox.Name = "lbltextbox";
+            this.lbltextbox.Size = new System.Drawing.Size(150, 13); // Tamaño del Label
+            this.lbltextbox.TabIndex = 2;
+            this.lbltextbox.Text = "Mensajes Entrantes"; // Texto sobre el TextBox
+            this.lbltextbox.BackColor = System.Drawing.Color.Transparent; 
+            this.lbltextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // txtbox
+            // 
+            this.txtbox.Location = new System.Drawing.Point(350, 140); // Ajustar ubicación según sea necesario
+            this.txtbox.Size = new System.Drawing.Size(400, 400); // Ajustar tamaño según sea necesario
+            this.txtbox.Multiline = true; // Permitir múltiples líneas
+            this.txtbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical; // Barra de desplazamiento vertical
+            this.txtbox.ReadOnly = true; // Solo lectura
+            this.txtbox.BackColor = System.Drawing.Color.LightGreen; // Color de fondo
+            this.txtbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle; // Estilo del borde
+            this.txtbox.Name = "txtbox";
+            this.txtbox.TabIndex = 4; // Ajustar el índice según el orden deseado
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.lblMessage);
+            this.ClientSize = new System.Drawing.Size(800, 600);
+
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.btnShowMessage);
+            this.Controls.Add(this.lbltextbox);
+            this.Controls.Add(this.txtbox);
             this.Name = "Form1";
-            this.Text = "Mi Aplicación";
+            this.Text = "Chat";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
--- Form1.cs
+++ Form1.cs
@@ -13,7 +13,29 @@ namespace Tarea_Extraclase_1_2022155662
         private void btnShowMessage_Click(object sender, EventArgs e)
         {
             string inputText = txtInput.Text;
-            lblMessage.Text = inputText;
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                // Agregar el texto a txtbox con un salto de línea
+                txtbox.AppendText(inputText + Environment.NewLine);
+                txtInput.Clear();
+            }
+            
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un mensaje.");
+            }
+        }
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verificar si la tecla presionada es Enter
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Evitar el sonido de "ding" al presionar Enter
+                e.SuppressKeyPress = true;
+
+                // Llamar al método para enviar el mensaje
+                btnShowMessage_Click(sender, e);
+            }
         }
     }
 }
