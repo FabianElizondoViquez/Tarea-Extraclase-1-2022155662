56b65e68756115b6c198a383bcc81270f4f853b0
Fabián <fabilu242003@gmail.com>
2024-07-30 20:36:22 -0600

    GUI Creada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Tarea_Extraclase_1_2022155662
-{
+{ 
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
@@ -8,6 +8,9 @@
         private TextBox txtbox;
         private Label lbltextbox;
         private Label lblinputbox;
+        private TextBox txtPuerto;
+        private Label lblpuerto;
+        private Label lblredactar;
         
 
         protected override void Dispose(bool disposing)
@@ -25,12 +28,15 @@
             this.txtInput = new System.Windows.Forms.TextBox();
             this.lbltextbox = new System.Windows.Forms.Label(); 
             this.txtbox = new System.Windows.Forms.TextBox();
-            this.lblinputbox = new System.Windows.FormsLabel();
+            this.lblinputbox = new System.Windows.Forms.Label();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.lblpuerto = new System.Windows.Forms.Label(); 
+            this.lblredactar = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnShowMessage
             // 
-            this.btnShowMessage.Location = new System.Drawing.Point(70, 490);
+            this.btnShowMessage.Location = new System.Drawing.Point(70, 410);
             this.btnShowMessage.Name = "btnShowMessage";
             this.btnShowMessage.Size = new System.Drawing.Size(200, 50);
             this.btnShowMessage.TabIndex = 0;
@@ -41,19 +47,20 @@
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(40, 250);
+            this.txtInput.Location = new System.Drawing.Point(40, 140);
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(270, 200);
+            this.txtInput.Size = new System.Drawing.Size(270, 240);
             this.txtInput.TabIndex = 1;
             this.txtInput.Multiline = true; // Permitir múltiples líneas
             this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical; // Barra de desplazamiento vertical
             this.txtInput.WordWrap = true; // Asegura que el texto se ajuste al tamaño del TextBox
             this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown); // Suscribir al evento KeyDown
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // lbltextbox
             // 
             this.lbltextbox.AutoSize = true; // Ajusta automáticamente el tamaño del Label
-            this.lbltextbox.Location = new System.Drawing.Point(350, 110); // Posición encima del TextBox
+            this.lbltextbox.Location = new System.Drawing.Point(350, 20); // Posición encima del TextBox
             this.lbltextbox.Name = "lbltextbox";
             this.lbltextbox.Size = new System.Drawing.Size(150, 13); // Tamaño del Label
             this.lbltextbox.TabIndex = 2;
@@ -63,28 +70,64 @@
             // 
             // txtbox
             // 
-            this.txtbox.Location = new System.Drawing.Point(350, 140); // Ajustar ubicación según sea necesario
-            this.txtbox.Size = new System.Drawing.Size(400, 400); // Ajustar tamaño según sea necesario
+            this.txtbox.Location = new System.Drawing.Point(350, 60); // Ajustar ubicación según sea necesario
+            this.txtbox.Size = new System.Drawing.Size(350, 400); // Ajustar tamaño según sea necesario
             this.txtbox.Multiline = true; // Permitir múltiples líneas
             this.txtbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical; // Barra de desplazamiento vertical
             this.txtbox.ReadOnly = true; // Solo lectura
-            this.txtbox.BackColor = System.Drawing.Color.LightGreen; // Color de fondo
             this.txtbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle; // Estilo del borde
             this.txtbox.Name = "txtbox";
             this.txtbox.TabIndex = 4; // Ajustar el índice según el orden deseado
+            this.txtbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(160, 80);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(150, 30);
+            this.txtPuerto.TabIndex = 1;
+            this.txtPuerto.Multiline = true; // Permitir múltiples líneas
+            this.txtPuerto.WordWrap = true; // Asegura que el texto se ajuste al tamaño del TextBox
+            this.txtPuerto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown); // Suscribir al evento KeyDown
+            this.txtPuerto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPuerto.TextAlign = HorizontalAlignment.Center;
+            // 
+            // lblpuerto
+            // 
+            this.lblpuerto.AutoSize = true; // Ajusta automáticamente el tamaño del Label
+            this.lblpuerto.Location = new System.Drawing.Point(40, 85); // Posición encima del TextBox
+            this.lblpuerto.Name = "lblpuerto";
+            this.lblpuerto.Size = new System.Drawing.Size(150, 13); // Tamaño del Label
+            this.lblpuerto.TabIndex = 2;
+            this.lblpuerto.Text = "Puerto destino:"; // Texto sobre el TextBox
+            this.lblpuerto.BackColor = System.Drawing.Color.Transparent; 
+            this.lblpuerto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // lblredactar
+            // 
+            this.lblredactar.AutoSize = true; // Ajusta automáticamente el tamaño del Label
+            this.lblredactar.Location = new System.Drawing.Point(35, 20); // Posición encima del TextBox
+            this.lblredactar.Name = "lblredactar";
+            this.lblredactar.Size = new System.Drawing.Size(150, 13); // Tamaño del Label
+            this.lblredactar.TabIndex = 2;
+            this.lblredactar.Text = "Redactar mensaje:"; // Texto sobre el TextBox
+            this.lblredactar.BackColor = System.Drawing.Color.Transparent; 
+            this.lblredactar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(800, 600);
-
+            this.ClientSize = new System.Drawing.Size(720, 500);
             this.Controls.Add(this.txtInput);
             this.Controls.Add(this.btnShowMessage);
             this.Controls.Add(this.lbltextbox);
             this.Controls.Add(this.txtbox);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.lblpuerto);
+            this.Controls.Add(this.lblredactar);
             this.Name = "Form1";
             this.Text = "Chat";
             this.ResumeLayout(false);
             this.PerformLayout();
-        }
+        } 
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -8,15 +8,24 @@ namespace Tarea_Extraclase_1_2022155662
         public Form1()
         {
             InitializeComponent();
+            this.txtPuerto.KeyPress += new KeyPressEventHandler(this.txtPuerto_KeyPress);
         }
 
         private void btnShowMessage_Click(object sender, EventArgs e)
         {
             string inputText = txtInput.Text;
-            if (!string.IsNullOrEmpty(inputText))
+            string Puerto = txtPuerto.Text;
+            if (!string.IsNullOrEmpty(Puerto + inputText))
             {
+                if (string.IsNullOrEmpty(Puerto) || string.IsNullOrEmpty(inputText))
+                {
+                    // Muestra un mensaje de advertencia si alguno de los campos está vacío
+                    MessageBox.Show("Por favor, ingrese tanto el puerto como el mensaje.");
+                    return; // Sale del método para evitar el envío del mensaje
+                }
                 // Agregar el texto a txtbox con un salto de línea
-                txtbox.AppendText(inputText + Environment.NewLine);
+                txtbox.AppendText("De: " + "<" + Puerto +  ">" + Environment.NewLine + inputText + Environment.NewLine + Environment.NewLine);
+                txtPuerto.Clear();
                 txtInput.Clear();
             }
             
@@ -35,6 +44,15 @@ namespace Tarea_Extraclase_1_2022155662
 
                 // Llamar al método para enviar el mensaje
                 btnShowMessage_Click(sender, e);
+            }
+        }
+        private void txtPuerto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Comprueba si la tecla presionada es un dígito o una tecla de control (como borrar)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                // Si no es un dígito ni una tecla de control, cancela el evento
+                e.Handled = true;
             }
         }
     }
